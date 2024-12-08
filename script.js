28b04a2f8901e9d064df39dfec8605e3c6b8cede
JIMMY JAYANT <113120690+jimmyjayant@users.noreply.github.com>
2024-12-08 23:05:39 +0530

    The event listener js code for various buttons on the calculator has been added. Now when you press the respective keys on the keyboard, the respective button on calculator will be pressed and become active with its hover and focus style sheets. The title attributes has been added to all the calculator's buttons.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var res = document.getElementById("result");
-var len = res.innerHTML.length;
-var char = res.innerHTML.charAt(len-1);
+var len = res.textContent.length;
+var char = res.textContent.charAt(len-1);
 
 window.addEventListener("resize", function() {
   if(window.innerWidth > 600)
@@ -9,230 +9,362 @@ window.addEventListener("resize", function() {
   }
 });
 
+/* My own code when the respective key is pressed then the respective css hover effect will take place of the key. */
+function mouseOver(a)
+{
+  /* The css hover effect for Enter (=) key is different from others as it is styled differently from others. */
+  if(a == 27)
+  {
+    document.getElementsByClassName("button")[a].style.color = "white";
+    document.getElementsByClassName("button")[a].style.backgroundColor = "darkblue";
+  }
+  else
+  {
+    /* The css styling for the rest of the buttons on the calculator. */
+    document.getElementsByClassName("button")[a].style.color = "white";
+    document.getElementsByClassName("button")[a].style.backgroundColor = "#111";
+  }
+}
+
+/* My own code after the key is pressed and css hover effect take place then after that the respective button is returned to its normal css style state. */
+function mouseOut(a)
+{
+  if(a == 27)
+    {
+      let red = 0;
+      let green = 0;
+      let blue = 255;
+      let alpha = 0.7;
+      let rgbaColor = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+      document.getElementsByClassName("button")[a].style.backgroundColor = rgbaColor;
+    }
+    else
+    {
+      document.getElementsByClassName("button")[a].style.color = "black";
+      document.getElementsByClassName("button")[a].style.backgroundColor = "#f1f1f1";
+    }
+}
+
 window.addEventListener("keydown", function(event) {
   switch(event.key)
   {
     case "Enter":
       event.preventDefault();
+      mouseOver(27);
       display();
+      setTimeout(() => mouseOut(27), 100);
+      console.log(len);
+      console.log(char);
       break;
     case "0":
       event.preventDefault();
+      mouseOver(25);
       zero();
+      setTimeout(() => mouseOut(25), 100);
+      console.log(len);
+      console.log(char);
       break;
     case "1":
       event.preventDefault();
+      mouseOver(20);
       one();
+      setTimeout(() => mouseOut(20), 100);
+      console.log(len);
+      console.log(char);
       break;
     case "2":
       event.preventDefault();
+      mouseOver(21);
       two();
+      setTimeout(() => mouseOut(21), 100);
+      console.log(len);
+      console.log(char);
       break;
     case "3":
       event.preventDefault();
+      mouseOver(22);
       three();
+      setTimeout(() => mouseOut(22), 100);
+      console.log(len);
+      console.log(char);
       break;
     case "4":
       event.preventDefault();
+      mouseOver(16);
       four();
+      setTimeout(() => mouseOut(16), 100);
+      console.log(len);
+      console.log(char);
       break;
     case "5":
       event.preventDefault();
+      mouseOver(17);
       five();
+      setTimeout(() => mouseOut(17), 100);
+      console.log(len);
+      console.log(char);
       break;
     case "6":
       event.preventDefault();
+      mouseOver(18);
       six();
+      setTimeout(() => mouseOut(18), 100);
+      console.log(len);
+      console.log(char);
       break;
     case "7":
       event.preventDefault();
+      mouseOver(12);
       seven();
+      setTimeout(() => mouseOut(12), 100);
+      console.log(len);
+      console.log(char);
       break;
     case "8":
       event.preventDefault();
+      mouseOver(13);
       eight();
+      setTimeout(() => mouseOut(13), 100);
+      console.log(len);
+      console.log(char);
       break;
     case "9":
       event.preventDefault();
+      mouseOver(14);
       nine();
+      setTimeout(() => mouseOut(14), 100);
+      console.log(len);
+      console.log(char);
       break;
     case "Backspace":
       event.preventDefault();
+      mouseOver(3);
       del();
+      setTimeout(() => mouseOut(3), 100);
+      console.log(char);
+      console.log(len);
       break;
     case "Delete":
       event.preventDefault();
+      mouseOver(0);
       clearResult();
+      setTimeout(() => mouseOut(0), 100);
+      console.log(char);
+      console.log(len);
       break;
     case "+":
       event.preventDefault();
+      mouseOver(19);
       plus();
+      setTimeout(() => mouseOut(19), 100);
+      console.log(char);
+      console.log(len);
       break;
     case "-":
       event.preventDefault();
+      mouseOver(15);
       minus();
+      setTimeout(() => mouseOut(15), 100);
+      console.log(char);
+      console.log(len);
       break;
     case "/":
       event.preventDefault();
+      mouseOver(7);
       divide();
+      setTimeout(() => mouseOut(7), 100);
+      console.log(char);
+      console.log(len);
       break;
     case "*":
       event.preventDefault();
+      mouseOver(11);
       mul();
+      setTimeout(() => mouseOut(11), 100);
+      console.log(char);
+      console.log(len);
       break;
     case ".":
       event.preventDefault();
+      mouseOver(26);
       dot();
+      setTimeout(() => mouseOut(26), 100);
+      console.log(char);
+      console.log(len);
       break;
     case "%":
       event.preventDefault();
+      mouseOver(1);
       percent();
+      setTimeout(() => mouseOut(1), 100);
+      console.log(char);
+      console.log(len);
       break;
     case "(":
       event.preventDefault();
+      mouseOver(9);
       leftbrac();
+      setTimeout(() => mouseOut(9), 100);
+      console.log(char);
+      console.log(len);
       break;
     case ")":
       event.preventDefault();
+      mouseOver(10);
       rightbrac();
+      setTimeout(() => mouseOut(10), 100);
+      console.log(char);
+      console.log(len);
       break;
   }
 });
 
 function clearResult() {
-  res.innerHTML = "";
+  res.textContent = "";
 }
 
 function divide() {
   if(char != "/")
-  {
-    res.innerHTML += "/";
-  }
+    {
+      res.textContent += "/";
+    }  
 }
 
 function mul() {
   if(char != "*")
   {
-    res.innerHTML += "*";
+    res.textContent += "*";
   }
 }
 
 function del() {
-  if(res.innerHTML != "")
+  if(res.textContent != "")
   {
-    res.innerHTML = res.innerHTML.slice(0,-1);
+    res.textContent = res.textContent.slice(0,-1);
   }
 }
 
 function squareroot() {
-  if(res.innerHTML != "")
-  res.innerHTML = Math.sqrt(eval(res.innerHTML));
+  if(res.textContent != "")
+  {
+    res.textContent = Math.sqrt(eval(res.textContent));
+  }
 }
 
 function cbroot() {
-  if(res.innerHTML != "")
-  res.innerHTML = Math.cbrt(eval(res.innerHTML));
+  if(res.textContent != "")
+  {
+    res.textContent = Math.cbrt(eval(res.textContent));
+  }
 }
 
 function seven() {
-  res.innerHTML += "7";
+  res.textContent += "7";
 }
 
 function eight() {
-  res.innerHTML += "8";
+  res.textContent += "8";
 }
 
 function nine() {
-  res.innerHTML += "9";
+  res.textContent += "9";
 }
 
 function minus() {
   if(char != "-")
   {
-    res.innerHTML += "-";
+    res.textContent += "-";
   }
 }
 
 function four() {
-  res.innerHTML += "4";
+  res.textContent += "4";
 }
 
 function five() {
-  res.innerHTML += "5";
+  res.textContent += "5";
 }
 
 function six() {
-  res.innerHTML += "6";
+  res.textContent += "6";
 }
 
 function plus() {
   if(char != "+")
   {
-    res.innerHTML += "+";
+    res.textContent += "+";
   }
 }
 
 function one() {
-  res.innerHTML += "1";
+  res.textContent += "1";
 }
 
 function two() {
-  res.innerHTML += "2";
+  res.textContent += "2";
 }
 
 function three() {
-  res.innerHTML += "3";
+  res.textContent += "3";
 }
 
 function dot() {
   if(char != ".")
   {
-    res.innerHTML += ".";
+    res.textContent += ".";
   }
 }
 
 function zero() {
-  res.innerHTML += "0";
+  res.textContent += "0";
+}
+
+function doublezero() {
+  res.textContent += "00";
 }
 
 function leftbrac() {
-  res.innerHTML += "(";
+  res.textContent += "(";
 }
 
 function rightbrac() {
-  res.innerHTML += ")";
+  res.textContent += ")";
 }
 
 function percent() {
-  res.innerHTML += '%';
+  res.textContent += '%';
 }
 
 function reciprocal() {
-  if(res.innerHTML == '')
+  if(res.textContent == '')
   {
-    res.innerHTML = "";
+    res.textContent = "";
   }
   else
   {
-    res.innerHTML = 1/(res.innerHTML);
+    res.textContent = (1/(res.textContent));
   }
 }
 
 function sqr() {
-  if(res.innerHTML != "")
+  if(res.textContent != "")
   {
-    res.innerHTML = eval(res.innerHTML);
-    res.innerHTML = Math.pow((res.innerHTML), 2);
+    res.textContent = eval(res.textContent);
+    res.textContent = Math.pow((res.textContent), 2);
   }
 }
 
 function display() {
-  res.innerHTML = (res.innerHTML).replace("%", "/100");
-  let final = eval(res.innerHTML);
+  res.textContent = (res.textContent).replace("%", "/100");
+  let final = eval(res.textContent);
   if(final)
-  res.innerHTML = final;
+  {
+    res.textContent = final;
+  }
   else
-  res.innerHTML = "";
+  {
+    res.textContent = "";
+  }
 }
 
 function nav()
@@ -245,3 +377,14 @@ function closenav()
   document.getElementsByClassName("navbar")[0].style.width = "0";
 }
 
+function x_to_power_y()
+{
+  res.textContent += "^"; // Better code to be discussed
+}
+
+function reversesign() {
+  if(res.textContent != "")
+  {
+    // Code yet to be discussed
+  }
+}
